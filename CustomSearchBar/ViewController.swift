f58e60b017f25b39cd5b114b8345df1e70786a38
Chan Hong Wing <jason021075@gmail.com>
2019-04-03 17:15:21 +0800

    Finish search function in Tutorial (Swift 4.2)

--- CustomSearchBar/ViewController.swift
+++ CustomSearchBar/ViewController.swift
@@ -9,8 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     
+    
+    var searchController: UISearchController!
     @IBOutlet weak var tblSearchResults: UITableView!
+    
+    var dataArray = [String]()
+    
+    var filteredArray = [String]()
+    
+    var shouldShowSearchResults = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +27,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tblSearchResults.delegate = self
         tblSearchResults.dataSource = self
+        
+        loadListOfCountries()
+        
+        configureSearchController()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,19 +47,104 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        if shouldShowSearchResults {
+            return filteredArray.count
+        }
+        else {
+            return dataArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "idCell", for: indexPath as IndexPath)
         
+        if shouldShowSearchResults {
+            cell.textLabel?.text = filteredArray[indexPath.row]
+        }
+        else {
+            cell.textLabel?.text = dataArray[indexPath.row]
+        }
+        
         return cell
     }
     
 
-    
-    private func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 60.0
     }
+
+    
+    func loadListOfCountries() {
+        // Specify the path to the countries list file.
+        let pathToFile = Bundle.main.path(forResource: "countries", ofType: "txt")
+        
+        if let path = pathToFile {
+            // Load the file contents as a string.
+            let countriesString = try! String(contentsOfFile: path, encoding: String.Encoding.utf8)
+            
+            // Append the countries from the string to the dataArray array by breaking them using the line change character.
+            dataArray = countriesString.components(separatedBy: "\n")
+            
+            // Reload the tableview.
+            tblSearchResults.reloadData()
+        }
+    }
+}
+
+extension ViewController: UISearchResultsUpdating, UISearchBarDelegate{
+    
+
+    
+    func configureSearchController() {
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        
+        //輸入搜尋關鍵字的時候讓整個view的背景變得黯淡
+        searchController.dimsBackgroundDuringPresentation = true
+        searchController.searchBar.placeholder = "Search here..."
+        
+        searchController.searchBar.delegate = self
+        
+        
+        //輸入搜尋關鍵字的時候讓整個view的背景變得黯淡
+        searchController.searchBar.sizeToFit()
+        //搜尋列顯示在tableview中
+        tblSearchResults.tableHeaderView = searchController.searchBar
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = true
+        tblSearchResults.reloadData()
+    }
+    
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        shouldShowSearchResults = false
+        tblSearchResults.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tblSearchResults.reloadData()
+        }
+        searchController.searchBar.resignFirstResponder()
+    }
+    
+    //輸入文字的當下即會觸發
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let searchString = searchController.searchBar.text else {
+            return
+        }
+        
+        // Filter the data array and get only those countries that match the search text.
+        filteredArray = dataArray.filter { $0.contains(searchString) }
+        
+        // Reload the tableview.
+        tblSearchResults.reloadData()
+    }
+
+
 }
 
