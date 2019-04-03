0968b4005b2b11119d19ce75f8e194b2cafb2e96
Chan Hong Wing <jason021075@gmail.com>
2019-04-03 15:52:29 +0800

    start project

--- CustomSearchBar/ViewController.swift
+++ CustomSearchBar/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tblSearchResults: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tblSearchResults.delegate = self
+        tblSearchResults.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: UITableView Delegate and Datasource functions
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "idCell", for: indexPath as IndexPath)
+        
+        return cell
+    }
+    
 
+    
+    private func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
+    }
 }
 
